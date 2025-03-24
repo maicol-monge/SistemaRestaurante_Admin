d6c2af9598abd5d08d4834cc792228189d2cc41e
jeannethesali03 <eyleen.salinas@catolica.edu.sv>
2025-03-24 11:48:50 -0600

    Vista 7 - Crear Menú (eyleen)

---
+++ SistemaRestaurante_Admin/Controllers/Menu/MenuController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaRestaurante_Admin.Controllers.Menu
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CrearMenu()
+        {
+            return View("/Views/Menu/CrearMenu.cshtml");
+        }
+    }
+}
