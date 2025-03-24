8871370b4d7edb55d11b61dbd9f2138c30101fb1
Ever Zamora <ever.zamora@catolica.edu.sv>
2025-03-24 11:46:26 -0600

    Vistas de gestion de mesas y cargos de empleados

---
+++ SistemaRestaurante_Admin/Controllers/Cargos/CargosController.cs
@@ -0,0 +1,17 @@
+﻿ using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaRestaurante_Admin.Controllers.Cargos
+{
+    public class CargosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CrearCargos()
+        {
+            return View();
+        }
+    }
+}
