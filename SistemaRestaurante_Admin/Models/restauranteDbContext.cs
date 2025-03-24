24bef356823988f8dc173b5fbf2e60694b829527
Maicol Monge <maicol.monge@catolica.edu.sv>
2025-03-23 19:11:26 -0600

    Add project files.

---
+++ SistemaRestaurante_Admin/Models/restauranteDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Sistema_de_Restaurante___Modulo_de_Administracion.Models
+{
+	public class restauranteDbContext : DbContext
+
+	{
+		public restauranteDbContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		//Las tablas con sus modelos aqui
+
+
+	}
+}
