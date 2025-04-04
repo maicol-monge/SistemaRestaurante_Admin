22e0406a84e1b6b9d0331ad31c3eb397a8dea3c7
Donis004 <donisbrenda004@gmail.com>
2025-04-04 16:36:41 -0600

    Creación de Combos finalizada. Validaciones incluidas

--- SistemaRestaurante_Admin/Models/restauranteDbContext.cs
+++ SistemaRestaurante_Admin/Models/restauranteDbContext.cs
@@ -15,11 +15,13 @@ namespace Sistema_de_Restaurante___Modulo_de_Administracion.Models
 		//Las tablas con sus modelos aqui :)
 		public DbSet<Empleados> Empleados { get; set; }
 		public DbSet<Platos> Platos { get; set; }
-		public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Combos> Combos { get; set; }
+        public DbSet<Platos_Combos> Platos_Combo { get; set; }
 
-		public DbSet<cargo> cargo { get; set; }
+
+        public DbSet<cargo> cargo { get; set; }
         public DbSet<mesas> mesas { get; set; }
-
 
     }
 }
